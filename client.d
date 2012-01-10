455e248b149f7c9bfbdf8ab8140441834af11093
Chris Manning <cmanning999@gmail.com>
2012-01-10 19:43:52 +0000

    Add some basic stuff

--- client.d
+++ client.d
@@ -1,5 +1,11 @@
 import std.socket;
 
 int main(string[] args) {
+    Socket sock = new TcpSocket;
+    sock.connect(new InternetAddress("127.0.0.1",4321));
+    while(sock.isAlive()) {
+        break;
+    }
+    sock.close();
     return 0;
 }
--- server.d
+++ server.d
@@ -1,5 +1,23 @@
-import std.socket;
+import std.socket,std.stdio,std.getopt,std.conv;
+
+ushort port = 4321;
+bool verbose, debugging;
 
 int main(string[] args) {
+    getopt(args,"port|p",&port,
+                "verbose|v",&verbose,
+                "debugging|d",&debugging);
+
+    Socket listener = new TcpSocket;
+    listener.bind(new InternetAddress(port));
+    listener.listen(10);
+
+    while(true) {
+        auto sn = listener.accept();
+        writefln("Connection from %s established.", to!string(sn.remoteAddress()));
+        sn.close();
+        break;
+    }
+
     return 0;
 }
